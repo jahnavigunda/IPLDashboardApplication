b2e7486c7e961429d91c2512028b3c9d9118c920
jahnavigunda <jahnavi.gunda93@gmail.com>
2021-08-02 12:49:43 -0700

    Added React Routes

--- src/frontend/frontend/src/App.js
+++ src/frontend/frontend/src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage />
+      <Router>
+         <Route path="/teams/:teamName">
+            <TeamPage />
+         </Route>
+      </Router>
     </div>
   );
 }
--- src/frontend/frontend/src/pages/TeamPage.js
+++ src/frontend/frontend/src/pages/TeamPage.js
@@ -1,11 +1,12 @@
 import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
 
    const [team, setTeam] = useState({matches: []});
-
+   const { teamName } = useParams();
 //this executes when component loads.
 //here, fetchMatches is async function which calls the rest api using fetch fn.
  //await - as fetch is returning some response.
@@ -13,7 +14,7 @@ export const TeamPage = () => {
    useEffect(
        () => {
        const fetchMatches = async() => {
-            const response = await fetch("http://localhost:8081/team/Rajasthan Royals");
+            const response = await fetch(`http://localhost:8081/team/${teamName}`);
             const data = await response.json();
             console.log(data);
             setTeam(data);
@@ -21,6 +22,9 @@ export const TeamPage = () => {
        fetchMatches();
        }, []
    );
+   if(!team || !team.teamName){
+     return <h1>Team Not Found</h1>
+   }
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
@@ -30,5 +34,6 @@ export const TeamPage = () => {
     </div>
   );
 }
+
 
 
