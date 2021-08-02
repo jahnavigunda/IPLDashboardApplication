6a6aecc2f125e2593cf54f9986094679db3b2e32
jahnavigunda <jahnavi.gunda93@gmail.com>
2021-08-02 11:15:07 -0700

    added react for Teampage

--- src/frontend/frontend/src/pages/TeamPage.js
+++ src/frontend/frontend/src/pages/TeamPage.js
@@ -1,17 +1,31 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
+
+   const [team, setTeam] = useState({matches: []});
+
+//this executes when component loads.
+//here, fetchMatches is async function which calls the rest api using fetch fn.
+ //await - as fetch is returning some response.
+ //use @CrossOrigin in controller class to allow url to access the cross domains.
+   useEffect(
+       () => {
+       const fetchMatches = async() => {
+            const response = await fetch("http://localhost:8081/team/Rajasthan Royals");
+            const data = await response.json();
+            console.log(data);
+            setTeam(data);
+       };
+       fetchMatches();
+       }, []
+   );
   return (
     <div className="TeamPage">
-      <h1>Team Name</h1>
-      <h3>Latest Matches</h3>
-      <MatchDetailCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
-
+      <h1>{team.teamName}</h1>
+      <MatchDetailCard match={team.matches[0]}/>
+      {team.matches.slice(1).map(match => <MatchSmallCard  match={match} />)}
 
     </div>
   );
